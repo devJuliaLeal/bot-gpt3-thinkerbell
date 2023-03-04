10d1bc886e68b670e10e6869ff85f98c3e3da2ec
Julia Leal <120651477+devJuliaLeal@users.noreply.github.com>
2023-03-04 17:01:48 -0300

    commit

--- imports.py
+++ imports.py
@@ -1,5 +1,4 @@
 from commands import (cute, joke, portugues)
-from text import texto1
 from keys import API_TOKEN
 from keys import BOT_TOKEN
 
--- main.py
+++ main.py
@@ -8,10 +8,19 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(content_types=['text'])
 def handle_chat(message):
     question = message.text[6:]
-    response = openai.Completion.create(engine="text-davinci-003", prompt=question+ cute, temperature=0.9,
+    response = openai.Completion.create(engine="text-davinci-003", prompt=question + cute, temperature=0.9,
                                         max_tokens=1999)
     answer = response['choices'][0]['text']
     bot.send_message(message.chat.id, answer)
+
+
+@bot.message_handler(commands=['N'])
+def handle_chat(message):
+    question = message.text[6:]
+    response = openai.Completion.create(engine="text-davinci-003", prompt=question , temperature=0.9,
+                                        max_tokens=1999)
+    answer = response ['choices'][0]['text']
+    bot.send_message(message.cha.id, answer)
 
 
 @bot.message_handler(commands=['J'])
